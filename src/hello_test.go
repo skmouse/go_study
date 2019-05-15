fda5d81ecbb18dfee934af1a900b64e7ff4a7db5
mac.mouse <jmuecho@gmail.com>
2019-05-15 22:26:12 +0800

    hello world

--- src/hello_test.go
+++ src/hello_test.go
@@ -2,6 +2,12 @@ package hello
 
 import "testing"
 
+/**
+ * go 语言测试文件的编写
+ * 1.源文件以_test.go结尾：xxx_test.go
+ * 2.测试方法名以Test开头：func TestXXX(t *testing.T){...}
+ */
+
 func TestHello(t *testing.T) {
 	t.Log("hello world")
 }
